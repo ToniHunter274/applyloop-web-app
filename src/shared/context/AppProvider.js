4fd36db81418a2ff0ebce8298d86f9f22f4fd92c
ToniHunter274 <toniprime15@gmail.com>
2026-08-20 20:18:18 +0100

    chore: remove legacy restaurant and vendor code

--- src/shared/context/AppProvider.js
+++ src/shared/context/AppProvider.js
@@ -1,25 +1,12 @@
 import React from 'react';
-import { CartProvider } from '../hooks/useCart';
-import { VendorProfileProvider } from '../hooks/useVendorProfile';
 import { ThemeProvider } from '../hooks/useTheme';
-import { VendorStatusProvider } from '../hooks/useVendorStatus';
 
-/**
- * AppProvider combines all our context providers into a single component
- * This makes it easier to wrap the application with all required providers
- */
 const AppProvider = ({ children }) => {
   return (
     <ThemeProvider>
-      <VendorProfileProvider>
-        <VendorStatusProvider>
-          <CartProvider>
-            {children}
-          </CartProvider>
-        </VendorStatusProvider>
-      </VendorProfileProvider>
+      {children}
     </ThemeProvider>
   );
 };
 
-export default AppProvider; 
+export default AppProvider;
